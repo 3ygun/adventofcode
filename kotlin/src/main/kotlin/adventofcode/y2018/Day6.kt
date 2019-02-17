2ff74642ba33429ce44036f5f8474ea918efaf53
3ygun <dsoller@outlook.com>
2019-02-17 18:00:14 -0500

    [kotlin] - 2018, Day 6, Star 1 - Rewrite to extensions
    Rewrite to extensions so that Day 6, Star 2 can utalize the base Grid also.

--- kotlin/src/main/kotlin/adventofcode/y2018/Day6.kt
+++ kotlin/src/main/kotlin/adventofcode/y2018/Day6.kt
@@ -21,23 +21,28 @@ object Day6 : Day {
     fun star1Calc(input: List<String>): Int {
         if (input.isEmpty()) return 0
 
-        val nextName = (0 until input.size).iterator()
-        val points = input.map { rawPoint ->
-                val points = rawPoint.split(",", limit = 2)
-                Point(
-                    x = points[0].trim().toInt(),
-                    y = points[1].trim().toInt(),
-                    name = nextName.nextInt()
-                )
-            }
-
-        val grid = Grid.of(points)
+        val points = parsePoints(input)
+        val grid = Grid(points)
         grid.populate()
 //        grid.print()
 
         return grid.largestArea()
     }
+
+    private fun parsePoints(input: List<String>): List<Point> {
+        val nextName = (0 until input.size).iterator()
+        return input.map { rawPoint ->
+            val points = rawPoint.split(",", limit = 2)
+            Point(
+                x = points[0].trim().toInt(),
+                y = points[1].trim().toInt(),
+                name = nextName.nextInt()
+            )
+        }
+    }
 }
+
+private typealias GridArray = Array<Array<GridPoint?>>
 
 private data class Point(val x: Int, val y: Int, val name: Int)
 
@@ -53,54 +58,51 @@ private data class GridPoint(val name: Int, val distance: Int) {
     }
 }
 
-private class Grid private constructor(
-    private val points: List<Point>,
-    private val grid: GridArray,
-    val width: Int,
-    val height: Int
+private data class Grid(
+    val points: List<Point>
 ) {
-    fun largestArea(): Int {
-        val counts = IntArray(points.size)
+    val width: Int = points.maxBy { it.x }!!.x + 1 // + 1 to account for the 0 start
+    val height: Int = points.maxBy { it.y }!!.y + 1 // + 1 to account for the 0 start
 
-        grid.forEach { row -> row.forEach { point ->
-            if (!point!!.isContended()) {
-                counts[point.name] = counts[point.name] + 1
-            }
-        } }
+    // Height, Width because the top left corner is 0, 0
+    private val grid: GridArray = Array(height) { Array<GridPoint?>(width) { null } }
 
-        val infinite = HashSet<Int>()
-        (0 until width).forEach { x ->
-            at(x, 0)?.run { infinite.add(this.name) }
-            at(x, height-1)?.run { infinite.add(this.name) }
-        }
-        (0 until height).forEach { y ->
-            at(0, y)?.run { infinite.add(this.name) }
-            at(width-1, y)?.run { infinite.add(this.name) }
-        }
+    fun at(x: Int, y: Int): GridPoint? = grid[y][x]
 
-        return counts
-            .mapIndexed { index, count -> Pair(index, count) }
-            .filter { !infinite.contains(it.first) }
-            .maxBy { it.second }!!
-            .second
-    }
+    fun claim(x: Int, y: Int, name: Int, distance: Int): Boolean {
+        val existing = at(x, y)
+            ?: return claimLocation(x, y, GridPoint(name, distance))
 
-    fun print() {
-        grid.forEach { row ->
-            row.forEach { point -> print("%3d".format(point?.name ?: -2)) }
-            println()
+        // we don't have to care about the distance in our algorithm
+        if (existing.name == name) return true
+
+        return when(existing.distanceCompareTo(distance)) {
+            1 -> claimLocation(x, y, GridPoint(name, distance))
+            0 -> claimLocation(x, y, GridPoint.Flag(distance))
+            else -> false
         }
     }
 
-    fun printDistance() {
-        grid.forEach { row ->
-            row.forEach { point -> print("%3d".format(point?.distance ?: -1)) }
-            println()
-        }
+    private fun claimLocation(x: Int, y: Int, gridPoint: GridPoint): Boolean {
+        grid[y][x] = gridPoint
+        return true // Claimed
     }
 
-    fun populate() {
-        for (point in points) {
+    fun print() = grid.forEach { row ->
+        row.forEach { point -> print("%3d".format(point?.name ?: -2)) }
+        println()
+    }
+
+    fun printDistance() = grid.forEach { row ->
+        row.forEach { point -> print("%3d".format(point?.distance ?: -1)) }
+        println()
+    }
+}
+
+//<editor-fold desc="Star 1">
+
+private fun Grid.populate() {
+    for (point in points) {
 //            println("${point.name} with ${point.x}, ${point.y}")
 //            print()
 //            println()
@@ -108,96 +110,86 @@ private class Grid private constructor(
 //            printDistance()
 //            println()
 
-            val name = point.name
-            // Initial Point
-            claimTerritory(point.x, point.y, name, distance = 0) ?: continue
+        val name = point.name
+        // Initial Point
+        claim(point.x, point.y, name, distance = 0)
 
-            // Top Left Corner
-            claimAllFor(
-                point = point,
-                xRange = point.x downTo 0,
-                yRange = point.y downTo 0
-            ) { x, y -> (point.x - x) + (point.y - y) }
+        // Top Left Corner
+        claimAllFor(
+            point = point,
+            xRange = point.x downTo 0,
+            yRange = point.y downTo 0
+        ) { x, y -> (point.x - x) + (point.y - y) }
 
-            // Top Right Corner
-            claimAllFor(
-                point = point,
-                xRange = point.x until width,
-                yRange = point.y downTo 0
-            ) { x, y -> (x - point.x) + (point.y - y) }
+        // Top Right Corner
+        claimAllFor(
+            point = point,
+            xRange = point.x until width,
+            yRange = point.y downTo 0
+        ) { x, y -> (x - point.x) + (point.y - y) }
 
-            // Bottom Left Corner
-            claimAllFor(
-                point = point,
-                xRange = point.x downTo 0,
-                yRange = point.y until height
-            ) { x, y -> (point.x - x) + (y - point.y) }
+        // Bottom Left Corner
+        claimAllFor(
+            point = point,
+            xRange = point.x downTo 0,
+            yRange = point.y until height
+        ) { x, y -> (point.x - x) + (y - point.y) }
 
-            // Bottom Right Corner
-            claimAllFor(
-                point = point,
-                xRange = point.x until width,
-                yRange = point.y until height
-            ) { x, y -> (x - point.x) + (y - point.y) }
+        // Bottom Right Corner
+        claimAllFor(
+            point = point,
+            xRange = point.x until width,
+            yRange = point.y until height
+        ) { x, y -> (x - point.x) + (y - point.y) }
+    }
+}
+
+private inline fun Grid.claimAllFor(
+    point: Point,
+    xRange: IntProgression,
+    yRange: IntProgression,
+    distanceCalc: (Int, Int) -> Int // Passing x, y
+) {
+    for (x in xRange) {
+        for (y in yRange) {
+            if (x == point.x && y == point.y) continue
+
+            val madeClaim = claim(x, y, point.name, distance = distanceCalc(x, y))
+            if (madeClaim) continue
+
+            if (y == point.y) return
+            break // otherwise we hit the a overflow on y
         }
     }
+}
 
-    private inline fun claimAllFor(
-        point: Point,
-        xRange: IntProgression,
-        yRange: IntProgression,
-        distanceCalc: (Int, Int) -> Int // Passing x, y
-    ) {
-        for (x in xRange) {
-            for (y in yRange) {
-                if (x == point.x && y == point.y) continue
+private fun Grid.largestArea(): Int {
+    val counts = IntArray(points.size)
 
-                val claim = claimTerritory(x, y, point.name, distance = distanceCalc(x, y))
-                if (claim != null) continue
-
-                if (y == point.y) return
-                break // otherwise we hit the a overflow on y
+    for (x in 0 until width) {
+        for (y in 0 until height) {
+            val point = at(x, y)
+            if (point != null && !point.isContended()) {
+                counts[point.name] = counts[point.name] + 1
             }
         }
     }
 
-    private fun claimTerritory(
-        x: Int,
-        y: Int,
-        name: Int,
-        distance: Int
-    ): GridPoint? {
-        val existing = at(x, y)
-            ?: return claim(x, y, GridPoint(name, distance))
-
-        // we don't have to care about the distance in our algorithm
-        if (existing.name == name) return existing
-
-        return when(existing.distanceCompareTo(distance)) {
-            1 -> claim(x, y, GridPoint(name, distance))
-            0 -> claim(x, y, GridPoint.Flag(distance))
-            else -> null
-        }
+    val infinite = HashSet<Int>()
+    (0 until width).forEach { x ->
+        at(x, 0)?.run { infinite.add(this.name) }
+        at(x, height-1)?.run { infinite.add(this.name) }
+    }
+    (0 until height).forEach { y ->
+        at(0, y)?.run { infinite.add(this.name) }
+        at(width-1, y)?.run { infinite.add(this.name) }
     }
 
-    private fun at(x: Int, y: Int): GridPoint? = grid[y][x]
-
-    private fun claim(x: Int, y: Int, gridPoint: GridPoint): GridPoint {
-        grid[y][x] = gridPoint
-        return gridPoint
-    }
-
-    companion object {
-        fun of(points: List<Point>): Grid {
-            val width = points.maxBy { it.x }!!.x + 1 // + 1 to account for the 0 start
-            val height = points.maxBy { it.y }!!.y + 1 // + 1 to account for the 0 start
-
-            // Height, Width because the top left corner is 0, 0
-            val grid = Array(height) { Array<GridPoint?>(width) { null } }
-            return Grid(points, grid, width, height)
-        }
-
-    }
+    return counts
+        .mapIndexed { index, count -> Pair(index, count) }
+        .filter { !infinite.contains(it.first) }
+        .maxBy { it.second }!!
+        .second
 }
 
-private typealias GridArray = Array<Array<GridPoint?>>
+//</editor-fold>
