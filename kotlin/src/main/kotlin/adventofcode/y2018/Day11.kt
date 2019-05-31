915e96aa4f977b917c5e11b5aea3164d9b09f2c7
3ygun <dsoller@outlook.com>
2019-05-31 00:03:37 -0400

    [kotlin] - 2018, Day 11, Star 1 & 2 cleanup
    Simplify and combine code for the 2 methods

--- kotlin/src/main/kotlin/adventofcode/y2018/Day11.kt
+++ kotlin/src/main/kotlin/adventofcode/y2018/Day11.kt
@@ -24,18 +24,31 @@ object Day11 : Day {
         return "The X,Y coordinate is: ${result.tlx},${result.tly},${result.size} (producing total power of ${result.totalPower})"
     }
 
-    internal fun star1Calc(gridSerialNumber: Int): Day11Star1Result {
+    internal data class Day11Result(
+        val tlx: Int,
+        val tly: Int,
+        val size: Int,
+        val totalPower: Int
+    )
+
+    private fun createBoard(gridSerialNumber: Int): Array<IntArray> {
         val board = Array(300) { IntArray(300) }
 
         // Fill in the board
-        (0 until 300).forEach { y ->
-            (0 until 300).forEach { x ->
+        for (y in 0 until 300) {
+            for (x in 0 until 300) {
                 board[y][x] = star1PointCalc(x, y, gridSerialNumber)
             }
         }
 
+        return board
+    }
+
+    internal fun star1Calc(gridSerialNumber: Int): Day11Result {
+        val board = createBoard(gridSerialNumber)
+
         // Calculate
-        var result = Day11Star1Result(0, 0, 0)
+        var result = Day11Result(0, 0, 0, 0)
         (0 until 298).forEach { y ->
             (0 until 298).forEach { x ->
                 var squareResult = 0
@@ -46,7 +59,7 @@ object Day11 : Day {
                 }
 
                 if (result.totalPower < squareResult) {
-                    result = Day11Star1Result(x, y, squareResult)
+                    result = Day11Result(x, y, 3, squareResult)
                 }
             }
         }
@@ -78,71 +91,18 @@ object Day11 : Day {
         return powerLevel.toInt() - 5
     }
 
-    internal data class Day11Star1Result(
-        val tlx: Int,
-        val tly: Int,
-        val totalPower: Int
-    )
-
-    internal fun star2Calc4(gridSerialNumber: Int): Day11Star2Result {
-        val board = Array(300) { IntArray(300) }
-
-        // Fill in the board
-        (0 until 300).forEach { y ->
-            (0 until 300).forEach { x ->
-                board[y][x] = star1PointCalc(x, y, gridSerialNumber)
-            }
-        }
+    internal fun star2Calc(gridSerialNumber: Int): Day11Result {
+        val board = createBoard(gridSerialNumber)
 
         // Calculate
-        var result = Day11Star2Result(0, 0, 0, 0)
-        for (y in 299 downTo 0) {
-            for (x in 299 downTo 0) {
-                for (side in 1 .. (299 - Math.max(y, x))) {
-                    var squareResult = 0
-                    for (sy in 0 until side) {
-                        for (sx in 0 until side) {
-                            squareResult += board[y + sy][x + sx]
-                        }
-                    }
-
-                    if (result.totalPower < squareResult) {
-                        result = Day11Star2Result(x, y, side, squareResult)
-                    }
-                }
-            }
-            println("y = $y")
-        }
-
-        return result
-    }
-
-    internal data class Day11Star2Result(
-        val tlx: Int,
-        val tly: Int,
-        val size: Int,
-        val totalPower: Int
-    )
-
-    internal fun star2Calc(gridSerialNumber: Int): Day11Star2Result {
-        val board = Array(300) { IntArray(300) }
-
-        // Fill in the board
-        (0 until 300).forEach { y ->
-            (0 until 300).forEach { x ->
-                board[y][x] = star1PointCalc(x, y, gridSerialNumber)
-            }
-        }
-
-        // Calculate
-        val results = Channel<Day11Star2Result>()
+        val results = Channel<Day11Result>()
         for (y in 299 downTo 0) {
             GlobalScope.launch {
                 results.send(calculate(y, board))
             }
         }
 
-        var result = Day11Star2Result(0, 0, 0, 0)
+        var result = Day11Result(0, 0, 0, 0)
         runBlocking {
             repeat(300) {
                 val aResult = results.receive()
@@ -154,8 +114,8 @@ object Day11 : Day {
         return result
     }
 
-    private fun calculate(y: Int, board: Array<IntArray>): Day11Star2Result {
-        var result = Day11Star2Result(0, 0, 0, 0)
+    private fun calculate(y: Int, board: Array<IntArray>): Day11Result {
+        var result = Day11Result(0, 0, 0, 0)
         for (x in 299 downTo 0) {
             for (side in 1 .. (299 - Math.max(y, x))) {
                 var squareResult = 0
@@ -166,7 +126,7 @@ object Day11 : Day {
                 }
 
                 if (result.totalPower < squareResult) {
-                    result = Day11Star2Result(x, y, side, squareResult)
+                    result = Day11Result(x, y, side, squareResult)
                 }
             }
         }
--- kotlin/src/test/kotlin/adventofcode/y2018/Day11Tests.kt
+++ kotlin/src/test/kotlin/adventofcode/y2018/Day11Tests.kt
@@ -17,19 +17,19 @@ class Day11Tests : StringSpec({
     }
     "Day 11, Star 1" {
         forall(
-            row(Day11.Day11Star1Result(33, 45, 29), 18),
-            row(Day11.Day11Star1Result(21, 61, 30), 42),
-            row(Day11.Day11Star1Result(235, 31, 31), Day11.STAR1)
-        ) { result: Day11.Day11Star1Result, gridSerialNumber: Int ->
+            row(Day11.Day11Result(33, 45, 3, 29), 18),
+            row(Day11.Day11Result(21, 61, 3, 30), 42),
+            row(Day11.Day11Result(235, 31,3,  31), Day11.STAR1)
+        ) { result: Day11.Day11Result, gridSerialNumber: Int ->
             Day11.star1Calc(gridSerialNumber) shouldBe result
         }
     }
     "Day 11, Star 2" {
         forall(
-            row(Day11.Day11Star2Result(90, 269, 16, 113), 18),
-            row(Day11.Day11Star2Result(232, 251, 12, 119), 42),
-            row(Day11.Day11Star2Result(241, 65, 10, 73), Day11.STAR2)
-        ) { result: Day11.Day11Star2Result, gridSerialNamber: Int ->
+            row(Day11.Day11Result(90, 269, 16, 113), 18),
+            row(Day11.Day11Result(232, 251, 12, 119), 42),
+            row(Day11.Day11Result(241, 65, 10, 73), Day11.STAR2)
+        ) { result: Day11.Day11Result, gridSerialNamber: Int ->
             Day11.star2Calc(gridSerialNamber) shouldBe result
         }
     }
