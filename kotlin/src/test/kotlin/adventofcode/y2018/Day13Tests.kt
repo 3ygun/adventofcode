5f8d19caf12059bcd4a938f4fa2c367381856903
3ygun <dsoller@outlook.com>
2019-08-10 22:52:27 -0400

    [kotlin] - Day13Tests change to individual tests
    Changes from 2 tests to N tests which  makes things easier to read.

--- kotlin/src/test/kotlin/adventofcode/y2018/Day13Tests.kt
+++ kotlin/src/test/kotlin/adventofcode/y2018/Day13Tests.kt
@@ -1,28 +1,28 @@
 package adventofcode.y2018
 
-import io.kotlintest.data.forall
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.StringSpec
+import io.kotlintest.specs.FunSpec
 import io.kotlintest.tables.row
 
-class Day13Tests : StringSpec({
-    "Day 13, Star 1" {
-        forall(
-            row(0 to 3, DAY13_STAR1_TEST_A),
-            row(2 to 0, DAY13_STAR1_TEST_B),
-            row(7 to 3, DAY13_STAR1_TEST_C),
-            row(0 to 0, DAY13_STAR1_TEST_D),
-            row(38 to 72, Day13.STAR1)
-        ) { result: Pair<Int, Int>, input: List<String> ->
+class Day13Tests : FunSpec({
+    listOf(
+        row(0 to 3, DAY13_STAR1_TEST_A, "Test A"),
+        row(2 to 0, DAY13_STAR1_TEST_B, "Test B"),
+        row(7 to 3, DAY13_STAR1_TEST_C, "Test C"),
+        row(0 to 0, DAY13_STAR1_TEST_D, "Test D"),
+        row(38 to 72, Day13.STAR1, "Star 1")
+    ).map { (result: Pair<Int, Int>, input: List<String>, description: String) ->
+        test("Day 13, Star 1 : $description") {
             Day13.star1Calc(input) shouldBe result
         }
     }
-    "Day 13, Star 2" {
-        forall(
-            row(2 to 0, DAY13_STAR2_TEST_A),
-            row(6 to 4, DAY13_STAR2_TEST_B),
-            row(68 to 27, Day13.STAR2)
-        ) { result: Pair<Int, Int>, input: List<String> ->
+
+    listOf(
+        row(2 to 0, DAY13_STAR2_TEST_A, "Test A"),
+        row(6 to 4, DAY13_STAR2_TEST_B, "Test B"),
+        row(68 to 27, Day13.STAR2, "Star 2")
+    ).map { (result: Pair<Int, Int>, input: List<String>, description: String) ->
+        test("Day 13, Star 2 : $description") {
             Day13.star2Calc(input) shouldBe result
         }
     }
